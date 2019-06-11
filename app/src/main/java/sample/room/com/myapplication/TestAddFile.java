fe21afab3cfeb771e43d91b4efe87427a69f4b84
abhisheknchetu <abhishekn@hetu.com>
2019-06-11 10:47:21 +0530

    Test method created on Push code

--- app/src/main/java/sample/room/com/myapplication/TestAddFile.java
+++ app/src/main/java/sample/room/com/myapplication/TestAddFile.java
@@ -1,12 +1,14 @@
 package sample.room.com.myapplication;
 
+import android.util.Log;
+
 public class TestAddFile {
 
     private void testMethodOnPullCode(){
 
     }
     private void testMethodOnPushCode(){
-
+        Log.i("Hello", "Friends");
     }
 
 

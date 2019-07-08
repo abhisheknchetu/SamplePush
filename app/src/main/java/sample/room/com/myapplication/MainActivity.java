b1bafe1ccc064caee19a106383431d19f227c3cd
abhisheknchetu <abhishekn@hetu.com>
2019-07-08 08:50:32 +0530

    Add one more log here

--- app/src/main/java/sample/room/com/myapplication/MainActivity.java
+++ app/src/main/java/sample/room/com/myapplication/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i(TAG, "Hello World");
+		Log.i("Hey Man", "How are you doing");
     }
 
     private void testUpload(){
